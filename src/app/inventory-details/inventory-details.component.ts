7f55e82046e1618b7167783f004d29f8be3a232d
Prodyut Das <prodyut.das@capgemini.com>
2020-08-12 05:35:49 +0530

    update

--- src/app/inventory-details/inventory-details.component.ts
+++ src/app/inventory-details/inventory-details.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { UserService } from '../service/user-service';
 import { ActivatedRoute } from '@angular/router';
 import { RouteParamsService } from '../service/router-param-service';
-
 @Component({
   selector: 'app-inventory-details',
   templateUrl: './inventory-details.component.html',
@@ -11,28 +10,40 @@ import { RouteParamsService } from '../service/router-param-service';
 export class InventoryDetailsComponent implements OnInit {
   details: any;
   id: string;
+  numId: number;
 
-  constructor(private user: UserService, private route: ActivatedRoute, private service: RouteParamsService) {
+  constructor(private user: UserService, private route: ActivatedRoute, private service: RouteParamsService, private cd: ChangeDetectorRef) {
     route.params
       .subscribe(params => service.id
         .next(params && params['id'] || undefined));
     service.id.subscribe(res => {
       this.id = res;
+      this.cd.markForCheck();
     })
 
   }
 
   ngOnInit() {
-    this.user.getDetails().subscribe((details: any) => {
-      this.details = details;
-    });
-    window.onload = () => {
-      this.getinventoryItemsById();
+    if (this.id) {
+      this.getinventoryItemsById(this.id);
     }
   }
-  getinventoryItemsById() {
-    this.user.getinventoryItemsById(this.id).subscribe(inventoryItems => {
+  getinventoryItemsById(id: string) {
+    this.user.getinventoryItemsById(id).subscribe(inventoryItems => {
       this.details = inventoryItems;
+      this.numId = Number(this.details.id);
+    }, error=>{
+      if(error){
+        alert("list ended!")
+      }
     })
+  }
+  next(id: string) {
+    console.log(id);
+
+    let numId = Number(id);
+    numId = numId + 1;
+    this.getinventoryItemsById(numId.toString());
+    this.cd.markForCheck();
   }
 }
